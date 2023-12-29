2b7836ac90828aeccdfb86078c8972277aeeec8e
Artin Zareie <artin.zareie@yahoo.com>
2023-12-29 16:59:47 +0330

    V1

---
+++ lua/artin/remap.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>op", vim.cmd.Ex)
+vim.opt.clipboard = "unnamedplus"
+vim.wo.number = true
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "C-3", ":norm I#<CR>")
+vim.keymap.set("n", "<leader>t", ':vs | te<CR>^WL<CR>')
+
