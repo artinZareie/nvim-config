9b1c77ca635ef138053a622197251c945bbb9bd4
Artin Zarei <artin.zareie@yahoo.com>
2025-07-25 08:33:32 +0330

    Changed terminal open from <leader>t to <leader>ttt

---
+++ lua/custom/plugins/rainbow.lua
@@ -0,0 +1,6 @@
+-- lua/plugins/rainbow-delimiters.lua
+return {
+  "hiphish/rainbow-delimiters.nvim",
+  -- It's recommended to load this plugin after nvim-treesitter
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+}
